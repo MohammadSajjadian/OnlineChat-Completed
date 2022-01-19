f847eddee671a5f259998b0d9734f16bb70ff262
Mohammad Sajjadian <86977415+MohammadSajjadian@users.noreply.github.com>
2022-01-19 16:04:06 +0330

    Add project files.

---
+++ SignalR/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalR.Areas.Identity.Data;
+using SignalR.Data;
+
+[assembly: HostingStartup(typeof(SignalR.Areas.Identity.IdentityHostingStartup))]
+namespace SignalR.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DBsignalR>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DBsignalRConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DBsignalR>();
+            });
+        }
+    }
+}
---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
